cce9ac43d62332ea1a6782966863055208ad5a48
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2020-11-05 15:52:36 +0000

    prepare if statement for when time is over

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,20 +1,25 @@
 /* ---------- Starting the game ---------- */
 
 // When clicking on the play button the start-game div disappears
-$('.start-game').on('click', function() {
-    $(this).remove();
+$(".start-game").on("click", function () {
+  $(this).remove();
 
-    // countdown also starts after clicking the play button
-    const startingTime = 1;
-    let time = startingTime * 60;
-    const countDown = document.getElementById('timer');
-    setInterval(startTime, 1000);
+  // countdown also starts after clicking the play button
+  const startingTime = 1;
+  let time = startingTime * 60;
+  const countDown = document.getElementById("timer");
+  setInterval(startTime, 1000);
 
-    function startTime() {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-        countDown.innerHTML = `${minutes}:${seconds}`;
-        time--;
+  function startTime() {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    countDown.innerHTML = `${minutes}:${seconds}`;
+    time--;
+    time = time < 0 ? 0 : time;
+
+    if (time === 0) {
+      console.log('Time is up');
     };
+  };
 });
