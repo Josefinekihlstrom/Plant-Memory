3080e56056c2d484eb3c6ce55b4ffd073b763ec9
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2020-11-05 13:30:59 +0000

    add timer function that starts when play button is clicked

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,20 @@
+/* ---------- Starting the game ---------- */
 
-
+// When clicking on the play button the start-game div disappears
 $('.start-game').on('click', function() {
     $(this).remove();
 
-    startTime();
-});
+    // countdown also starts after clicking the play button
+    const startingTime = 1;
+    let time = startingTime * 60;
+    const countDown = document.getElementById('timer');
+    setInterval(startTime, 1000);
 
-function startTime() {
-    console.log('timer started!');
-};
+    function startTime() {
+        const minutes = Math.floor(time / 60);
+        let seconds = time % 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        countDown.innerHTML = `${minutes}:${seconds}`;
+        time--;
+    };
+});
