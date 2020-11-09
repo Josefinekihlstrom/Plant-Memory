cfbfa52fd1e43e30afb3182b342cbde6d649574c
Josefinekihlstrom <josefine.kihlstrm@gmail.com>
2020-11-09 17:11:00 +0000

    rm timer to make new due to bug

--- assets/script/script.js
+++ assets/script/script.js
@@ -30,20 +30,8 @@ function playPause() {
 $(".start-game").on("click", function () {
   $(this).remove();
 
-  // timer also starts after clicking the play button
-  const startingTime = 0;
-  let time = startingTime * 60;
-  const countDown = document.getElementById("timer");
-  setInterval(startTime, 1000);
 
-  function startTime() {
-    const minutes = Math.floor(time / 60);
-    let seconds = time % 60;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    countDown.innerHTML = `${minutes}:${seconds}`;
-    time++;
-    time = time < 0 ? 0 : time;
-  };
+
 });
 
 /* ---------- Playing the game ---------- https://www.youtube.com/watch?v=ZniVgo8U7ek/ */
